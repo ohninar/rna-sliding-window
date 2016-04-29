4db2dec24c4233d5ccdab6adc1106ca947dd66be
Bruno Adonis <adonisdesa@hotmail.com>
2016-04-29 12:27:22 -0300

    RNA update
    correção

--- REDE_NEURAL_ARTIFICIAL/REDE_NEURAL/functionsLib/predict.m
+++ REDE_NEURAL_ARTIFICIAL/REDE_NEURAL/functionsLib/predict.m
@@ -11,6 +11,7 @@ num_labels = size(Theta2, 1);%pega a quantidade de linhas da matriz
 p = zeros(size(X, 1), 1); % 
 
 h1 = sigmoid([ones(m, 1) X] * Theta1');
+h2 = sigmoid([ones(m, 1) h1] * Theta2');
 [dummy, p] = max(h2, [], 2);
 
 end
